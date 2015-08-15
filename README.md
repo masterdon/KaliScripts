9289a405bd3b292d3369d4d8b8c9a4c5d268b0c8
Donald Raikes <donraikes1030@gmail.com>
2015-08-14 23:10:06 -0700

    Added recipes folder and skeleton file

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # KaliScripts
+#
+# These are a set of scripts I use to customize a build of Kali 2.0 for my own use.
+# Some of the scripts are based on tools described in The Hacker Playbook 2.
+
---
+++ recipes/skel.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#==============================================================================
+#	File:	
+#	Author: Donald Raikes <donraikes1030@gmail.com>
+#	Date:
+#	Copyright (c) 2015 Donald Raikes
+#	All rights reserved by the copyright holder.
+#==============================================================================
